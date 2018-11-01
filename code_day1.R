879e7ae8ba15ec7a2bf4209aa9d036cd266a3c04
Morgan Hughes <morgan.hughes@ufl.edu>
2018-11-01 14:14:50 -0400

    adding libraries

---
+++ code_day1.R
@@ -0,0 +1,14 @@
+#libraries needed
+library(dismo)
+library(raster)
+library(rJava)#for calling maxent from dismo package (need Java installed on computer)
+library(mgcv)#for gam
+library(PresenceAbsence)#for model evaluation
+library(randomForest)
+library(MuMIn)
+library(reshape2)
+library(maxnet)#maxent with maxnet
+library(glmnet)#needed for maxnet
+library(plyr)
+library(ggplot2)
+library(viridis)
